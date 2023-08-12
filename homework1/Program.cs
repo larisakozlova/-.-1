0bc56f26dc55831f4cab21ef941c9401cc827f9a
Larisa <larisa@kozlova.ru>
2023-08-12 12:24:43 +0300

    homework_1

---
+++ homework1/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдает, 
+// какое число большее, а какое меньшее.
+//  a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите число");
+//     int a = Convert.ToInt32(Console.ReadLine());
+
+//  Console.WriteLine("Введите число");
+//     int b = Convert.ToInt32(Console.ReadLine());
+
+//     int max = Math.Max(a, b); int min = Math.Min(a, b);
+
+//   Console.WriteLine($"Максимальное число: {max}");
+//   Console.WriteLine($"Минимальное число: {min}");
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите число:");
+//  int a = Convert.ToInt32(Console.ReadLine());
+ 
+//  Console.WriteLine("Введите число:");
+//   int b = Convert.ToInt32(Console.ReadLine());
+
+//   Console.WriteLine("Введите число:");
+//  int c = Convert.ToInt32(Console.ReadLine());
+
+//   int max = a;
+
+//     if (b > max)
+//  {
+//      max = b;
+//  }
+//     if (c > max)
+//  {
+//      max = c;
+//  }
+
+//  Console.WriteLine($"Максимальное число: {max}");
+
+
+
+// Задача 6. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+// (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Введите число: ");
+//   int number = Convert.ToInt32(Console.ReadLine());
+
+//   if (number % 2 == 0)
+//  {
+//      Console.WriteLine("Да");
+//  }
+//    else
+//  {
+//      Console.WriteLine("Нет");
+//  }
+
+
+
+// Задача 8.Напишите программу, которая на вход принимает число (N), а на выходе показывает 
+// все четные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+//     Console.WriteLine("Чётные числа: ");
+
+//         for (int i = 2; i <= N; i += 2)
+//         {
+//             Console.WriteLine(i);
+//         }
+
+
+
+
